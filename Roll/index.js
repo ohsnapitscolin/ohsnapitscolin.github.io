c33849a47d88ceb7428589a6d5ea67e19b1d9bd5
ohsnapitscolin <colin.dunn15@gmail.com>
2015-07-20 21:26:24 -0400

    added new things

---
+++ Roll/index.js
@@ -0,0 +1,60 @@
+var myRef = new Firebase('https://resistancewithjustin.firebaseio.com/');
+var missionRef = myRef.child('roll');
+
+missionRef.on("value", function(snapshot) {
+	$('#totalroll').html(snapshot.val().roll);
+}, function (errorObject) {
+	console.log("The read failed: " + errorObject.code);
+});
+
+
+function submitroll() {	 
+	var numDie = 0;
+	if ($('#1').is(':checked')) {
+		numDie = 1;	
+	}
+	else if ($('#2').is(':checked')) {
+		numDie = 2;	
+	}
+	else if ($('#3').is(':checked')) {
+		numDie = 3;	
+	}
+	else if ($('#4').is(':checked')) {
+		numDie = 4;	
+	}
+	else if ($('#5').is(':checked')) {
+		numDie = 5;	
+	}
+	else if ($('#6').is(':checked')) {
+		numDie = 6;	
+	}
+	else if ($('#7').is(':checked')) {
+		numDie = 7;	
+	}
+	else if ($('#8').is(':checked')) {
+		numDie = 8;	
+	}
+	var totalRoll = 0;
+	for (var i = 0; i < numDie; i++) {
+		var roll = Math.floor((Math.random() * 3));
+		totalRoll += roll;
+	}
+	missionRef.set({roll: totalRoll});
+
+}
+
+function updateStyle() {
+	for (var i = 1; i <= 8; i++) {
+		if ($('#' + i).is(':checked')) {
+			$('#' + i + 'Text').css({
+				'color': 'red',
+				'font-weight': 'bold'
+			});
+		} else {
+			$('#' + i + 'Text').css({
+				'color': 'inherit',
+				'font-weight': 'inherit'
+			});	
+		}
+	}
+}
